1012f87d68e1c4f26983e94c0039b0080f03b03a
spidercatnat <bloomjazzer@gmail.com>
2020-05-22 14:08:59 -0700

    Renames folders

--- 1-handling-data/1-1-load-csv/loadcsv.go
+++ 1-handling-data/1-1-load-csv/loadcsv.go
@@ -3,39 +3,31 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"log"
-	"os"
 	"strconv"
+
+	"github.com/spidercatnat/go-ml/1-handling-data/functions"
 )
 
-// In contrast to Python, Go will throw an error if our CSV data is corrupted.
-
 func main() {
-	// Open the CSV
-	f, err := os.Open("./load-csv/test.csv")
-	check(err)
+	f := functions.OpenFile("./data/test.csv")
+	defer f.Close()
 
 	// Read in the CSV records
 	r := csv.NewReader(f)
 	records, err := r.ReadAll()
-	check(err)
+	functions.Check(err)
 
 	// Get the maximum value in the integer column
 	var intMax int
 	for _, record := range records {
 		// Parse the integer value
 		intVal, err := strconv.Atoi(record[0])
-		check(err)
+		// In contrast to Python, Go will throw an error if our CSV data is does not fit the specified format.
+		functions.Check(err)
 		if intVal > intMax {
 			intMax = intVal
 		}
 	}
 
 	fmt.Println(intMax)
-}
-
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
 }
--- 1-handling-data/1-2-read-csv/main.go
+++ 1-handling-data/1-2-read-csv/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 
-	"github.com/spidercatnat/go-ml/01-handling-data/functions"
+	"github.com/spidercatnat/go-ml/1-handling-data/functions"
 )
 
 // Using the functions defined below, load CSV and read out all records
 func main() {
 	// Load CSV
-	f := functions.OpenFile("./read-csv/data/iris.csv")
+	f := functions.OpenFile("./data/iris.csv")
 	defer f.Close()
 
 	// Read all records iteratively
